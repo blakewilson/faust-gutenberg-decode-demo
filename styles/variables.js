9d2626755de9c5cdd7a469014ded437b154cd770
Blake Wilson <blake@blake.id>
2023-03-06 11:18:38 -0600

    Clean up styles

--- styles/variables.js
+++ styles/variables.js
@@ -1,23 +1,27 @@
-// Contains the Theme.js palette colors
+/**
+ * This file contains some theme.json colors and font sizes. We are still
+ * working on supporting theme.json, so in the meantime, we are creating a
+ * variables.js file to map those properties ourself.
+ */
 export const colorsMap = {
-  'primary': '#9DFF20',
-  'secondary': '#345C00',
-  'tertiary': '#f6f6f6',
-  'contrast': '#000000',
-  'foreground': '#000000',
-  'background': '#ffffff',
-  'cyan-bluish-gray': '#abb8c3',
-  'pale-pink': '#f78da7',
-  'luminous-vivid-amber': '#fcb900',
-  'pale-cyan-blue': '#8ed1fc',
-  'vivid-cyan-blue': '#0693e3',
-  'vivid-purple': '#9b51e0',
-  'vivid-red': '#cf2e2e',
-  'light-green-cyan': '#7bdcb5',
-}
+  primary: "#9DFF20",
+  secondary: "#345C00",
+  tertiary: "#f6f6f6",
+  contrast: "#000000",
+  foreground: "#000000",
+  background: "#ffffff",
+  "cyan-bluish-gray": "#abb8c3",
+  "pale-pink": "#f78da7",
+  "luminous-vivid-amber": "#fcb900",
+  "pale-cyan-blue": "#8ed1fc",
+  "vivid-cyan-blue": "#0693e3",
+  "vivid-purple": "#9b51e0",
+  "vivid-red": "#cf2e2e",
+  "light-green-cyan": "#7bdcb5",
+};
 
 export const fontSizesMap = {
-  'small': '1rem',
-  'normal': '1.5rem',
-  'large': '1.75rem'
-}
+  small: "1rem",
+  normal: "1.5rem",
+  large: "1.75rem",
+};
--- utils/getStyles.js
+++ utils/getStyles.js
@@ -2,14 +2,20 @@ import { colorsMap, fontSizesMap } from "../styles/variables.js";
 import { cssToReactStyle } from "./cssToReactStyle.js";
 import { compileCSS } from "@wordpress/style-engine";
 
+/**
+ * Transform an "attributes" object from WPGraphQL Content Blocks into a
+ * usable React "style" object with proper colors and font sizes from the variables.js file.
+ *
+ * @param {Object} attributes Attributes object for a Block from WPGraphQL Content Blocks
+ * @returns
+ */
 export default function getStyles(attributes) {
-  console.log(attributes.style);
-
   return {
     backgroundColor: colorsMap[attributes?.backgroundColor],
     color: colorsMap[attributes?.textColor],
     fontSize: fontSizesMap[attributes?.fontSize],
     borderColor: colorsMap[attributes?.borderColor],
-    ...(attributes.style && cssToReactStyle(compileCSS(attributes.style))),
+    ...(attributes.style &&
+      cssToReactStyle(compileCSS(JSON.parse(attributes.style)))),
   };
 }
